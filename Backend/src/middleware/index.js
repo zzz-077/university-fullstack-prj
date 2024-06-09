ab21d7489e24b53c2874bdc613406783b6a76a6a
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-06-09 21:50:28 +0400

    created routes,controllers,middlewear & have to fix "get user method" problem

--- Backend/src/middleware/index.js
+++ Backend/src/middleware/index.js
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
-import router from "../routes/userRoutes.js";
-// const router = express.Router();
+const router = express.Router();
 router.use(
   cors({
     origin: `http://localhost:${process.env.FRONTEND_PORT}`,
