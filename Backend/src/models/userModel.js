257e4960063369ce78e47c40df6639a36fb7c3f9
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-06-10 17:35:16 +0400

    fixed adding user in users collection

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -54,6 +54,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("users", userSchema);
+const User = mongoose.model("user", userSchema, "users");
 
 export default User;
