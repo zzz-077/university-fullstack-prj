aa922ad3736c9360396e067c8800e178b4650295
NikaEliashvili <nikaeliashvili18@outlook.com>
2024-06-13 23:07:40 +0400

    change token expiration dates

--- Backend/src/utils/generateToken.js
+++ Backend/src/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 const generateAccessToken = (userID) => {
   return jwt.sign({ userID }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "1hr",
   });
 };
 
 const generateRefreshToken = (userID) => {
   return jwt.sign({ userID }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "1d",
   });
 };
 
